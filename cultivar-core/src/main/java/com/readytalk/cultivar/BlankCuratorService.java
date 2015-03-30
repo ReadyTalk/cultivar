9cedf0755c330d1a1981520eb6d54a2cc2b3e408
David H. Clements <dclemen@mac.com>
2015-03-30 14:03:48 -0600

    Providing Inject annotation for BlankCuratorService.

--- cultivar-core/src/main/java/com/readytalk/cultivar/BlankCuratorService.java
+++ cultivar-core/src/main/java/com/readytalk/cultivar/BlankCuratorService.java
@@ -1,11 +1,18 @@
 package com.readytalk.cultivar;
 
 import com.google.common.util.concurrent.AbstractIdleService;
+import com.google.inject.Inject;
 
 /**
  * A class that does nothing to allow for servicemanager instantiation when nothing is configured.
  */
-class BlankCuratorService extends AbstractIdleService implements CuratorService {
+public class BlankCuratorService extends AbstractIdleService implements CuratorService {
+
+    @Inject
+    BlankCuratorService() {
+
+    }
+
     @Override
     protected void startUp() throws Exception {
 
